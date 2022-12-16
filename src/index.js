eb9b3f70c4b21a58f16edcca31301eaaa74d2989
oluwadamilolaoyelade <107127029+oluwadamilolaoyelade@users.noreply.github.com>
2022-12-16 11:01:48 +0100

    .

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "odyssey-lift-off-part5-server-production-c813.up.railway.app/", // change to YOUR own production server
   cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
 });
 
 ReactDOM.render(
